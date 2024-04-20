8d461432ed9a86ca22a762600a3da0b5a9c6e260
dinhd <dinhd@DESKTOP-7BK339H>
2024-04-20 14:24:55 +0700

    Add project files.

---
+++ LuyenTapKtra1/Controllers/HocSinhController.cs
@@ -0,0 +1,48 @@
+﻿using LuyenTapKtra1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuyenTapKtra1.Controllers
+{
+    public class HocSinhController : Controller
+    {
+        public List<HocSinh> _hocSinh;
+        public HocSinhController() {
+            _hocSinh = new List<HocSinh>()
+            {
+                new HocSinh("001", "Dinh Duy", "L001", 7, 8),
+                new HocSinh("002", "Dinh gg", "L002", 1, 2),
+                new HocSinh("003", "Dinh dd", "L001", 5, 8),
+                new HocSinh("004", "Dinh cc", "L003", 10, 9),
+            };
+        }
+        // GET: HocSinh
+        public ActionResult Index()
+        {
+            var List1 = _hocSinh.Where(x => x.diemTk < 13).ToList();
+            var List2 = _hocSinh.Where(x => x.Lop == "L001").ToList();
+            ViewBag.List1 = List1;
+            ViewBag.List2 = List2;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(HocSinh hocSinh)
+        {
+            return RedirectToAction("HienThiAdd", hocSinh);
+
+        }
+        public ActionResult HienThiAdd(HocSinh hocSinh)
+        {
+            return View(hocSinh);
+
+        }
+    }
+}
---
+++ LuyenTapKtra1/Models/HocSinh.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuyenTapKtra1.Models
+{
+    public class HocSinh
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Lop {  get; set; }
+        public int diemToan {  get; set; }
+        public int diemVan { get; set; }
+        public int diemTk
+        {
+            get
+            {
+                return diemToan * diemVan;
+            }
+        }
+        public HocSinh() { }
+        public HocSinh(string id, string name, string lop, int diemtoan, int diemvan)
+        {
+            Id = id;
+            Name = name;
+            Lop = lop;
+            diemToan = diemtoan;
+            diemVan = diemvan;
+
+
+        }
+    }
+}
